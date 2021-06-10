41c3fad4a6b0f6cea269823f34ec63bd6f12ae2b
Sitijs <sitijs118@gmail.com>
2021-06-11 00:22:53 +0700

    fix auth error user

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.use(cors());
 app.use(express.json());
 
 //middleware body parser
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '100mb'}));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb', extended: true}));
 
 //middleware
 app.use(express.json());
