756b5dbdf2540c9c792666a523d49ead865c617d
Sitijs <sitijs118@gmail.com>
2021-06-10 18:29:27 +0700

    add console log auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../models/User');
+const user = require('../models/User');
 const bcrypt = require('bcryptjs')
 const {registerValidation} = require('../validation');
 const {loginValidation} = require('../validation');
@@ -18,12 +18,17 @@ router.post('/register', async (req,res)=> {
 // res.send(validation);
 
 	const {error} = registerValidation(req.body);
-	if (error) return res.status(400).send(error.details[0].message);
+	if (error) {
+		return res.status(400).send(error.details[0].message);
+		console.log(error.details[0].message);
+	}
 
 	//check apakah user sudah berada di database
 	const emailExist = await User.findOne({email: req.body.email});
-	if (emailExist) return res.status(400).send('email sudah ada');
-
+	if (emailExist) {
+		return res.status(400).send('email sudah ada');
+		console.log('email sudah ada');
+	}
 
 	//hash the password
 	const salt = await bcrypt.genSalt(10);
@@ -44,6 +49,7 @@ router.post('/register', async (req,res)=> {
 		res.send({user: user._id});
 	}catch (err){
 		res.status(400).send(err);
+		console.log(err);
 	}
 });
 
@@ -51,17 +57,26 @@ router.post('/register', async (req,res)=> {
 	//login
 	router.post('/login', async (req,res)=> {
 		const {error} = loginValidation(req.body);
-		if (error) return res.status(400).send(error.details[0].message);
+		if (error) {
+			return res.status(400).send(error.details[0].message);
+			console.log(error.details[0].messag);
+		}
 			//check apakah user sudah berada di database
 			const user = await User.findOne({email: req.body.email});
-			if (!user) return res.status(400).send('email atau password salah');
+			if (!user) {
+				return res.status(400).send('email atau password salah');
+				console.log('email atau password salah');
+			}
 
 
 			//password is correct
 			const validPass = await bcrypt.compare(req.body.password, user.password);
-			if(!validPass) return res.status(400).send('invalid password')
+			if(!validPass) {
+				return res.status(400).send('invalid password');
+				console.log('invalid password');
+			}
 
-			res.send('login berhasil');
+			res.send({user: user._id});
 
 
 	});
