a83bde77a52974fe56813c3818248ba96e676d2e
Sitijs <sitijs118@gmail.com>
2021-06-13 02:53:14 +0700

    add trackrecord.js

---
+++ routes/recordTrackerRoute.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require ('mongoose');
+
+const recordTracker = require('../recordTracker')
+    //get last (Number of save document) record number by ID pasien
+    router.get('/Last/:ID', async (req,res) => {
+        try{
+            recordNum = await recordTracker.getNumberofRecord(req.query.userID);
+            //console.log("HAI" + recordNum)
+            res.json(recordNum); 
+        }catch(err){
+            console.log(err);
+            res.json({message: 'err GET Record Number'});
+        }
+    });
+    //Add record number by ID pasien
+    router.get('/Update/:ID', async (req,res) => {
+        try{
+            recordNum = await recordTracker.updateNumberofRecord(req.query.userID);
+            console.log("AKHIRNYA" + recordNum)
+            res.json(recordNum); 
+        }catch(err){
+            console.log(err);
+            res.json({message: 'err UPDATE Record Number'});
+        }
+    });
+module.exports = router;
