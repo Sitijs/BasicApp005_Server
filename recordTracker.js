4535a33be684eece0e00480ca8d8824a0434ee5e
Sitijs <sitijs118@gmail.com>
2021-06-13 10:46:29 +0700

    fix record tracker34

--- recordTracker.js
+++ recordTracker.js
@@ -11,7 +11,7 @@ async function getNumberofRecord(pasienID) {
 	}
 
 	var noRecord;
-	await User.findBy(query).limit(1).sort({$natural:-1})
+	await User.find(query).limit(1).sort({$natural:-1})
 	.then(result =>{
 		noRecord = result.noRecord;
 	})
